78eb3a78f11368385d9210559a75644bf5678d4c
Ekaterina Romanova <Dekaterina83@mail.ru>
2025-07-13 21:15:03 +0300

    file changed

--- calculator-appium-tests/src/test/java/ru/netology/qa/CalculatorTest.java
+++ calculator-appium-tests/src/test/java/ru/netology/qa/CalculatorTest.java
@@ -1,36 +1,59 @@
-package ru.netology.calculator;
+package ru.netology.qa;
 
 import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.MobileElement;
-import io.appium.java_client.pagefactory.AndroidFindBy;
-import io.appium.java_client.pagefactory.AppiumFieldDecorator;
-import io.appium.java_client.pagefactory.iOSXCUITFindBy;
-import org.openqa.selenium.support.PageFactory;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import ru.netology.qa.screens.MainScreen;
 
-import java.time.Duration;
+import java.net.MalformedURLException;
+import java.net.URL;
 
-public class MobileObjects {
+import static io.appium.java_client.remote.AndroidMobileCapabilityType.APP_ACTIVITY;
+import static io.appium.java_client.remote.AndroidMobileCapabilityType.APP_PACKAGE;
+import static io.appium.java_client.remote.IOSMobileCapabilityType.*;
+import static io.appium.java_client.remote.MobileCapabilityType.*;
+import static org.junit.jupiter.api.TestInstance.Lifecycle;
+import static org.openqa.selenium.remote.CapabilityType.PLATFORM_NAME;
 
-    @AndroidFindBy(id = "com.google.android.calculator:id/digit_2")
-    @iOSXCUITFindBy(xpath = "XCUIElement...")
-    public MobileElement btn2;
-
-    @AndroidFindBy(id = "com.google.android.calculator:id/op_add")
-    @iOSXCUITFindBy(xpath = "XCUIElement...")
-    public MobileElement btnPlus;
-
-    @AndroidFindBy(id = "com.google.android.calculator:id/eq")
-    @iOSXCUITFindBy(xpath = "XCUIElement...")
-    public MobileElement btnRavno;
-
-    @AndroidFindBy(id = "com.google.android.calculator:id/result_final")
-    @iOSXCUITFindBy(xpath = "XCUIElement...")
-    public MobileElement result;
+@TestInstance(Lifecycle.PER_CLASS)
+public class CalculatorTest {
 
     private AppiumDriver driver;
 
-    public MobileObjects(AppiumDriver driver) {
-        this.driver = driver;
-        PageFactory.initElements(new AppiumFieldDecorator(driver, Duration.ofSeconds(10)), this);
+    @BeforeAll
+    public void createDriver() throws MalformedURLException {
+        String platform = System.getProperty("platform");
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        if ("android".equals(platform)) {
+            desiredCapabilities.setCapability(PLATFORM_NAME, "android");
+            desiredCapabilities.setCapability(DEVICE_NAME, "any name");
+            desiredCapabilities.setCapability(APP_PACKAGE, "com.google.android.calculator");
+            desiredCapabilities.setCapability(APP_ACTIVITY, "com.android.calculator2.Calculator");
+        } else if ("ios".equals(platform)) {
+            desiredCapabilities.setCapability(PLATFORM_NAME, "iOS");
+            desiredCapabilities.setCapability(DEVICE_NAME, "iPhone 11");
+            desiredCapabilities.setCapability(BUNDLE_ID, "com.shubham-iosdev.Calculator-UI");
+            desiredCapabilities.setCapability(AUTOMATION_NAME, "XCUITest");
+        } else {
+            throw new IllegalArgumentException(String.format("Platform %s no supported", platform));
+        }
+        driver = new AppiumDriver(new URL("http://127.0.0.1:4723/wd/hub"), desiredCapabilities);
+    }
+
+    @Test
+    public void test() {
+        MainScreen mainScreen = new MainScreen(driver);
+        mainScreen.digitTwoButton.click();
+        mainScreen.plusSignButton.click();
+        mainScreen.digitTwoButton.click();
+        mainScreen.equalsSignButton.click();
+        Assertions.assertEquals("4", mainScreen.resultField.getText());
+    }
+
+    @AfterAll
+    public void quitDriver() {
+        if (driver != null) {
+            driver.quit();
+        }
     }
 }
